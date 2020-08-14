d683f45bacf3e555af77f5ab9ed6c292fbd22a78
Thomas Ingram <thomasingram@vertx.xyz>
2020-08-14 18:39:59 +1000

    Adding style extensions helper for UIElements

---
+++ Editor/Core/StyleExtensions.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Vertx.Editors.Editor
+{
+	public static class StyleExtensions
+	{
+		public static StyleSheet GetStyleSheet(string name)
+		{
+			string[] guids = AssetDatabase.FindAssets($"t:{nameof(StyleSheet)} {name}");
+			if (guids.Length == 0)
+				return null;
+			var sheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(AssetDatabase.GUIDToAssetPath(guids[0]));
+			return sheet;
+		}
+		
+		public static VisualTreeAsset GetUXML(string name)
+		{
+			string[] guids = AssetDatabase.FindAssets($"t:{nameof(VisualTreeAsset)} {name}");
+			if (guids.Length == 0)
+				return null;
+			var uxml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(AssetDatabase.GUIDToAssetPath(guids[0]));
+			return uxml;
+		}
+		
+		public static (StyleSheet, VisualTreeAsset) GetStyleSheetAndUXML(string name)
+		{
+			string[] guids = AssetDatabase.FindAssets(name);
+			if (guids.Length == 0)
+				return (null, null);
+			(StyleSheet, VisualTreeAsset) values = default;
+			foreach (string guid in guids)
+			{
+				string path = AssetDatabase.GUIDToAssetPath(guid);
+				if (path.EndsWith($"/{name}.uss"))
+					values.Item1 = AssetDatabase.LoadAssetAtPath<StyleSheet>(path);
+				else if (path.EndsWith($"/{name}.uxml"))
+					values.Item2 = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
+			}
+			return values;
+		}
+	}
+}
