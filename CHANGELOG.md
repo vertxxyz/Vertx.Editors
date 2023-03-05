f846cfe94160bb8f902d37d268fbc45978a56e58
Thomas Ingram <thomasingram@vertx.xyz>
2023-03-05 17:46:29 +1100

    Added shortcut to clear the console window (Ctrl+Alt+Shift+C)
    Fixed rare InvalidCastException.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.4.1]
+- Added shortcut to clear the console window (Ctrl+Alt+Shift+C)
+- Fixed rare InvalidCastException.
+
 ## [1.4.0] - 2023-02-20
 - Added Edit/Gizmos menu to mass-disable gizmo icons.
 
---
+++ Editor/Shortcuts/Shortcuts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+namespace Vertx.Editors
+{
+	internal static class Shortcuts
+	{
+		[Shortcut("Clear Console", KeyCode.C, ShortcutModifiers.Control | ShortcutModifiers.Alt | ShortcutModifiers.Shift)]
+		public static void ClearConsole() => Type.GetType("UnityEditor.LogEntries,UnityEditor")!.GetMethod("Clear", BindingFlags.Public | BindingFlags.Static)!.Invoke(null, null);
+	}
+}
